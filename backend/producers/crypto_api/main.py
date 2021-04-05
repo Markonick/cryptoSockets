10f2c6cad0088d499e629f91f989f7cbe7e363b0
markonick <nicolas.markos@gmail.com>
2021-04-06 01:47:19 +0300

    API error string index

--- backend/producers/crypto_api/main.py
+++ backend/producers/crypto_api/main.py
@@ -1,14 +1,8 @@
-import typing, time, abc
-import aiohttp
-import asyncio
-import json
-import os
+import typing, time, abc, aiohttp, asyncio, json, os
 from typing import Optional
-import json, os, asyncio, websockets
 from aiokafka import AIOKafkaProducer
 from aiokafka.errors import LeaderNotAvailableError
-from binance.client import Client
-from aiohttp import ClientSession
+
 
 # CONSTANTS
 CURRENCY = 'usdt'
@@ -42,7 +36,7 @@ KAFKA_CREATE_TOPICS = os.environ.get("KAFKA_CREATE_TOPICS")
 # INTERFACES / ABSTRACTIONS
 class ICryptoInstrument(abc.ABC):
     @abc.abstractmethod
-    async def _coro(self, symbol: str, currency: str) -> None:
+    async def _coro(self, params: dict, symbol: str, session) -> None:
         pass
 
     @abc.abstractmethod
@@ -50,7 +44,7 @@ class ICryptoInstrument(abc.ABC):
         pass
 
     @abc.abstractmethod
-    async def _get_kline_async(self, symbol: str, currency: str) -> None:
+    async def _get_kline_async(self, params: dict, session) -> None:
         pass
 
 
@@ -59,64 +53,110 @@ class ICryptoApiProducer(abc.ABC):
     async def produce(self, data: dict) -> None:
         pass
 
+class KlinesResponseMessage(object):   
+    @classmethod
+    def get_klines(cls, payload: dict, exchange: str, symbol: str) -> str:
+        klines = [cls._get_single_kline_to_dict(kline) for kline in payload]
+
+        reponse = {
+            "exchange": exchange,
+            "symbol": symbol,
+            "klines": klines,
+        }
+
+        return reponse
+    
+    def _get_single_kline_to_dict(kline: list) -> dict:
+        kline_keys = [
+            "open_time",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+            "close_time",
+            "quote_asset_volume",
+            "number_of_trades",
+            "taker_buy_base_asset_volume",
+            "taker_buy_quote_asset_volume",
+            "ignore"
+        ]
+
+        # kline_dict = { k: kline[i] for i, k in enumerate(kline_keys) }
+        kline_dict = {
+            "open_time": kline[0],
+            "open": kline[1],
+            "high": kline[2],
+            "low": kline[3],
+            "close": kline[4],
+            "volume": kline[5],
+            "close_time": kline[6],
+            "quote_asset_volume": kline[7],
+            "number_of_trades": kline[8],
+            "taker_buy_base_asset_volume": kline[9],
+            "taker_buy_quote_asset_volume": kline[10],
+            "ignore": kline[11],
+        }
+        return kline_dict
+
 # CONCRETE IMPLEMENTATIONS
 class CryptoInstrument(ICryptoInstrument):
     """
     Class that receives an injected producer, creates a coroutine per crypto symbol
-    by openining a websocket connection to an exchange endpoint 
-    and uses the injected producer to push stream data per crypto (per coroutine) to the Kafka topic.
+    by calling the API endpoint and uses the injected producer to push api data per 
+    crypto (per coroutine) to the Kafka topic.
     """
-    def __init__(self, producer: ICryptoApiProducer, endpoint: str, interval: str, exchange: str) -> None:
+    def __init__(self, producer: ICryptoApiProducer, endpoint: str, interval: str, exchange: str, start_ts: int, end_ts: int, limit: int) -> None:
         self.producer = producer
         self.endpoint = endpoint
         self.interval = interval
         self.exchange = exchange
+        self.start_ts = start_ts
+        self.end_ts = end_ts
+        self.limit = limit
 
     async def gather_instrument_coros(self) -> None:
-        coros = [self._coro(symbol, CURRENCY) for symbol in SYMBOLS]
-        await asyncio.gather(*coros)
+        async with aiohttp.ClientSession() as session:
+            coros = [
+                self._coro(self._createQueryParams(symbol, self.interval, self.limit, self.start_ts, self.end_ts), symbol, session) 
+                for symbol in SYMBOLS
+            ]
 
-    async def _coro(self, symbol: str, currency: str) -> None:
-        await self._get_kline_async(symbol, currency)
+            await asyncio.gather(*coros)
 
-    async def _get_kline_async(self, symbol: str, interval: str, start_ts: int, end_ts: int, limit: int, currency: str, session) -> None:
-        """
-        """
-        url = BINANCE_API_BASE_URL
-        payload = {
-            'symbol': symbol,
+    async def _coro(self, params: dict, symbol: str, session) -> None:
+        await self._push_to_kafka(params, symbol, session )
+
+    def _createQueryParams(self, symbol: str, interval: str, limit: int, start_ts: int=None, end_ts: int=None) -> dict:
+        """Prepare query params for klines api"""
+        return {
+            'symbol': f"{symbol}{CURRENCY}".upper(),
             'interval': interval,
-            # 'startTime': start_ts,
-            # 'endTime': end_ts,
+            'startTime': start_ts,
+            'endTime': end_ts,
             'limit': limit,
         }
-        try:
-            response = await session.request(method='GET', url=url, params=payload)
-            response.raise_for_status()
-            print(f"Response status ({url}): {response.status}")
-        except aiohttp.ClientConnectorError as http_err:
-            print(f"HTTP error occurred: {http_err}")
-        except Exception as err:
-            print(f"An error ocurred: {err}")
-        response_json = await response.json()
-        return response_json
+
+    async def _get_kline_async(self, params: dict, session) -> dict:
+        """Get async kline api. Returns array of klines for time window"""
+        async with session.get(endpoint) as response:
+            return await response.text()
     
-    async def run_program(self, symbol, interval, start_ts, end_ts, limit, currency, session):
-        """Wrapper for running program in an asynchronous manner"""
+    async def _push_to_kafka(self, params: dict, symbol: str, session) -> None:
+        """Wrapper for running program in an asynchronous manner, pushes api reposnse to kafka"""
         try:
-            response = await self._get_kline_async(symbol, currency, session)
-            data_json = json.dumps(response)
-            print(f"Response: {data_json}")
-            msg = {**data_json, "exchange": self.exchange}
-            await self.producer.produce(msg)
+            while True:
+                response = await self._get_kline_async(params, session)
+                msg = KlinesResponseMessage.get_klines(response, self.exchange, symbol)
+
+                await self.producer.produce(msg)
+                time.sleep(30)
         except Exception as err:
             print(f"Exception occured: {err}")
             pass
 
 class CryptoApiProducer(ICryptoApiProducer):
-    """
-    Class that creates a Kafka producer and pushes crypto related api data to a Kafka topic 
-    """
+    """Class that creates a Kafka producer and pushes crypto related api data to a Kafka topic"""
     async def produce(self, message: dict) -> None:
         producer = AIOKafkaProducer(bootstrap_servers=KAFKA_ADVERTISED_HOST_NAME)
 
@@ -127,10 +167,6 @@ class CryptoApiProducer(ICryptoApiProducer):
             value_json = json.dumps(message).encode('utf-8')
             # print("PRODUCING TICKER !!!!!!!!!!!!!!!!!: ", value_json)
             await producer.send_and_wait(KAFKA_CREATE_TOPICS, value_json)
-        # except LeaderNotAvailableError:
-        #     time.sleep(1)
-        #     value_json = json.dumps(message).encode('utf-8')
-        #     await producer.send_and_wait(KAFKA_CREATE_TOPICS, value_json)
         finally:
             # wait for all pending messages to be delivered or expire.
             await producer.stop()
@@ -140,11 +176,10 @@ if __name__ == '__main__':
     exchange = "binance"
     endpoint = f"{BINANCE_API_BASE_URL}/klines"
     interval = "1m"
-
+    start_ts = None
+    end_ts = None
+    limit = 1000
     producer = CryptoApiProducer()
-    instrument1 = CryptoInstrument(producer, endpoint, interval, exchange)
+    instrument = CryptoInstrument(producer, endpoint, interval, exchange, start_ts, end_ts, limit)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(instrument1.gather_instrument_coros())
-
-    async with ClientSession() as session:
-        await asyncio.gather(*[run_program(symbol, CURRENCY, session) for symbol in symbols])
+    loop.run_until_complete(instrument.gather_instrument_coros())
